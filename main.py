7c11659c59914580856fd75ef497d1a8e2f0af83
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2023-05-04 16:43:37 -0300

    atualização

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-valor1 = int(input('Digite valor 1:'))
-valor2 = int(input('Digite valor 2:'))
+from biblioteca import *
 
-soma (valor1,valor2)
+
+resultado_adicao = soma(10, 3)
+print(resultado_adicao) 
+
+resultado_subtracao = subtracao(15, 3)
+print(resultado_subtracao) 
+
+resultado_multiplicacao = multiplicacao(30, 3)
+print(resultado_multiplicacao) 
+
+resultado_divisao = divisao(10, 3)
+print(resultado_divisao) 
+
+resultado_modulo = modulo(10, 3)
+print(resultado_modulo) 
+
+resultado_divisao_inteira = divisao_inteira(20, 3)
+print(resultado_divisao_inteira) 
